ce2b9ba1969d0605b7027fa53d53ce00a69afc91
markijohn <kitae.john@gmail.com>
2023-01-20 17:58:33 +0900

    fasteval use for math eval expression

--- druid-xml/src/qwidget/drawable.rs
+++ druid-xml/src/qwidget/drawable.rs
@@ -1,4 +1,5 @@
 use std::borrow::Cow;
+use std::collections::BTreeMap;
 use druid::RoundedRectRadii;
 use druid::{Point,Size,Insets,Color,LinearGradient,RadialGradient, Widget, PaintCtx, Env, widget::BackgroundBrush, RenderContext};
 use druid::kurbo::{Line, Circle, Arc, RoundedRect, Ellipse};
@@ -24,16 +25,6 @@ impl Default for BorderStyle {
     }
 }
 
-#[derive(Clone,Copy)]
-pub enum Number {
-    Abs(u64), //absolute position
-    Rel(f64), //relative position
-    Calc(SimpleCalc)
-}
-
-#[derive(Clone,Debug)]
-pub struct InvalidNumberError;
-
 #[derive(Debug,Clone,Copy)]
 enum CalcOp {
     Add,
@@ -52,53 +43,48 @@ pub struct SimpleCalc {
 
 impl SimpleCalc {
     pub fn parse(s:&str) -> Result<Self,InvalidNumberError> {
-        let s = s.trim();
-        if s.starts_with("calc(") && s.ends_with(")") {
-            let mut split = s[5 .. s.rfind(')').unwrap()].split_whitespace();
-            let rel = split.next().ok_or_else(|| InvalidNumberError)?;
-            let op = split.next().ok_or_else(|| InvalidNumberError)?;
-            let abs = split.next().ok_or_else(|| InvalidNumberError)?;
+        let mut split = s[5 .. s.rfind(')').unwrap()].split_whitespace();
+        let rel = split.next().ok_or_else(|| InvalidNumberError)?;
+        let op = split.next().ok_or_else(|| InvalidNumberError)?;
+        let abs = split.next().ok_or_else(|| InvalidNumberError)?;
 
-            let (rel,is_rel) = if rel.ends_with('%') {
-                (rel[..rel.len()-1].parse::<f64>().map_err( |_| InvalidNumberError )? / 100f64, true)
-            } else if rel.find('.').is_some() {
-                (rel.parse::<f64>().map_err( |_| InvalidNumberError )?, true)
-            } else {
-                (rel.parse::<f64>().map_err( |_| InvalidNumberError )?, false)
-            };
-
-            
-            let (rel,op,abs) = if is_rel {
-                let abs = abs.parse::<f64>().map_err( |_| InvalidNumberError )?;
-                match op {
-                    "+" => (rel, CalcOp::Add, abs),
-                    "-" => (rel, CalcOp::Add, -abs),
-                    "*" => (rel, CalcOp::Multiply, abs),
-                    //"/" => (rel, CalcOp::Multiply, abs / 10f64.powi( (abs.log10().floor()+1f64) as _ ) ),
-                    "/" => (rel, CalcOp::Multiply, 1f64 / abs ),
-                    _ => return Err(InvalidNumberError)
-                }
-            } else {
-                let _abs = rel;
-                let rel = if abs.ends_with('%') {
-                    abs[..abs.len()-1].parse::<f64>().map_err( |_| InvalidNumberError )? / 100f64
-                } else if abs.find('.').is_some() {
-                    abs.parse::<f64>().map_err( |_| InvalidNumberError )?
-                } else {
-                    return Err(InvalidNumberError)
-                };
-                match op {
-                    "+" => (rel, CalcOp::Add, _abs),
-                    "-" => (-rel, CalcOp::Add, _abs),
-                    "*" => (rel, CalcOp::Multiply, _abs),
-                    "/" => (rel, CalcOp::Divide, _abs ),
-                    _ => return Err(InvalidNumberError)
-                }
-            };
-            Ok( Self {rel, op, abs} )
+        let (rel,is_rel) = if rel.ends_with('%') {
+            (rel[..rel.len()-1].parse::<f64>().map_err( |_| InvalidNumberError )? / 100f64, true)
+        } else if rel.find('.').is_some() {
+            (rel.parse::<f64>().map_err( |_| InvalidNumberError )?, true)
         } else {
-            Err(InvalidNumberError)
-        }
+            (rel.parse::<f64>().map_err( |_| InvalidNumberError )?, false)
+        };
+
+        
+        let (rel,op,abs) = if is_rel {
+            let abs = abs.parse::<f64>().map_err( |_| InvalidNumberError )?;
+            match op {
+                "+" => (rel, CalcOp::Add, abs),
+                "-" => (rel, CalcOp::Add, -abs),
+                "*" => (rel, CalcOp::Multiply, abs),
+                //"/" => (rel, CalcOp::Multiply, abs / 10f64.powi( (abs.log10().floor()+1f64) as _ ) ),
+                "/" => (rel, CalcOp::Multiply, 1f64 / abs ),
+                _ => return Err(InvalidNumberError)
+            }
+        } else {
+            let _abs = rel;
+            let rel = if abs.ends_with('%') {
+                abs[..abs.len()-1].parse::<f64>().map_err( |_| InvalidNumberError )? / 100f64
+            } else if abs.find('.').is_some() {
+                abs.parse::<f64>().map_err( |_| InvalidNumberError )?
+            } else {
+                return Err(InvalidNumberError)
+            };
+            match op {
+                "+" => (rel, CalcOp::Add, _abs),
+                "-" => (-rel, CalcOp::Add, _abs),
+                "*" => (rel, CalcOp::Multiply, _abs),
+                "/" => (rel, CalcOp::Divide, _abs ),
+                _ => return Err(InvalidNumberError)
+            }
+        };
+        Ok( Self {rel, op, abs} )
     }
 
     pub fn calc(&self, size:f64) -> f64 {
@@ -109,6 +95,17 @@ impl SimpleCalc {
         }
     }
 }
+
+#[derive(Clone)]
+pub enum Number {
+    Abs(u64), //absolute position
+    Rel(f64), //relative position
+    SimpleCalc(SimpleCalc),
+    Calc(String) //https://developer.mozilla.org/ko/docs/Web/CSS/calc
+}
+
+#[derive(Clone,Debug)]
+pub struct InvalidNumberError;
 
 impl Number {
     pub fn as_u64(&self) -> Option<u64> {
@@ -143,11 +140,12 @@ impl Number {
         }
     }
 
-    pub fn calc(&self, size:f64) -> f64 {
+    pub fn calc(&self, vars:&mut CalcVars) -> f64 {
         match self {
             Number::Abs(v) => *v as _,
-            Number::Rel(v) => size * *v,
-            Number::Calc(v) => v.calc(size)
+            Number::Rel(v) => *v * vars.base_size(),
+            Number::SimpleCalc(calc) => calc.calc( vars.base_size() ),
+            Number::Calc(v) => fasteval2::ez_eval(v, vars).unwrap()
         }
     }
 }
@@ -156,8 +154,9 @@ impl FromStr for Number {
     type Err = InvalidNumberError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let v = if let Ok(calc) = SimpleCalc::parse(s) {
-            Self::Calc(calc)
+        let s = s.trim();
+        let v = if s.starts_with("calc(") && s.ends_with(')') {
+            Self::Calc( s[5 .. s.len()-1].to_string() )
         } else if s.find('.').is_some() {
             Self::Rel( s.parse::<f64>().map_err( |_| InvalidNumberError )? )
         } else if s.ends_with('%') {
@@ -196,7 +195,7 @@ pub enum Pos {
     BottomRight { x:f64, y : f64 }
 }
 
-#[derive(Clone,Copy)]
+#[derive(Clone)]
 pub struct QPoint {
     x : Number,
     y : Number
@@ -207,28 +206,73 @@ impl QPoint {
         Ok( Self { x:Number::from_str(x)?, y:Number::from_str(y)? } )
     }
 
-    pub fn calc_rate(&self, width:f64, height:f64) -> (f64,f64) {
-        ( self.x.calc( width) , self.y.calc( height) )
+    pub fn calc(&self, map:&mut CalcVars) -> (f64,f64) {
+        ( self.x.calc( map) , self.y.calc( map) )
     }
 }
 
-#[derive(Clone)]
-pub enum PointEnd {
-    WidthHeight(QPoint), //width, height
-    RightBottom(QPoint) //right, bottom
+
+pub struct CalcVars {
+    width : f64,
+    height : f64,
+    time : f64,
+    is_width_base : bool,
+    buf : String
 }
 
-impl PointEnd {
-    pub fn calc(&self, start:(f64,f64), width:f64, height:f64) -> (f64,f64) {
-        match self {
-            Self::WidthHeight(point) => {
-                let calc = point.calc_rate(width, height);
-                ( start.0+calc.0, start.1+calc.1 )
-            },
-            Self::RightBottom(point) => {
-                let calc = point.calc_rate(width, height);
-                ( width-calc.0, height-calc.1 )
+impl CalcVars {
+    pub fn new(width:f64, height:f64, time:f64, is_width_base:bool) -> Self {
+        Self {
+            width, height, time, is_width_base, buf : String::new()
+        }
+    }
+
+    pub fn set_width_base(&mut self, flag:bool) {
+        self.is_width_base = flag;
+    }
+
+    pub fn width_base(&mut self) -> &mut Self {
+        self.is_width_base = true;
+        self
+    }
+
+    pub fn height_base(&mut self) -> &mut Self {
+        self.is_width_base = false;
+        self
+    }
+
+    pub fn base_size(&self) -> f64 {
+        if self.is_width_base {
+            self.width
+        } else {
+            self.height
+        }
+    }
+}
+
+impl fasteval2::evalns::EvalNamespace for CalcVars {
+    //more advanced function : https://github.com/izihawa/fasteval2/blob/master/examples/advanced-vars.rs
+    fn lookup(&mut self, name: &str, args: Vec<f64>, keybuf: &mut String) -> Option<f64> {
+        let mydata: [f64; 3] = [11.1, 22.2, 33.3];
+        match name {
+            // Custom constants/variables:
+            "width" => Some(self.width),
+            "height" => Some(self.height),
+            "time" => Some(self.time),
+            "size" => Some(self.base_size()),
+
+            // Custom function:
+            "sum" => Some(args.into_iter().fold(0.0, |s, f| s + f)),
+
+            _ => {
+                if name.starts_with("__inner_perc_") {
+                    Some( name[ "__inner_perc_".len() .. ].parse::<f64>().unwrap() * self.base_size() )
+                } else {
+                    None
+                }
             }
+
+            _ => None,
         }
     }
 }
@@ -258,7 +302,7 @@ pub enum Drawable {
     //Absolute line
     //start : absolute start point
     //end : absolute end point
-    Line{ start:Option<QPoint>, end:PointEnd, style:Option<BorderStyle> }
+    Line{ start:Option<QPoint>, end:QPoint, style:Option<BorderStyle> }
 }
 
 pub struct DrawableStack(Vec<Drawable>);
@@ -271,10 +315,13 @@ impl DrawableStack {
     pub fn draw(&self, ctx:&mut PaintCtx) {
         let mut last_point = (0., 0.);
         let mut last_style = Default::default();
+        let bounds = ctx.size().to_rect();
+        let width = bounds.width();
+        let height = bounds.height();
+        let time = 0f64;
+        let mut cvar = CalcVars::new(width, height, time, false);
+
         for d in self.0.iter() {
-            let bounds = ctx.size().to_rect();
-            let width = bounds.width();
-            let height = bounds.height();
             macro_rules! draw {
                 ($fill:ident, $shape:ident, $border:ident) => {
                     match $fill {
@@ -292,23 +339,23 @@ impl DrawableStack {
             match d {
                 Drawable::Rect { top, right, bottom, left, border, round, fill  } => {
                     let round = round.unwrap_or( Default::default() );
-                    let rect = RoundedRect::new( left.calc(width), top.calc(height), right.calc(width), bottom.calc(height), round );
+                    let rect = RoundedRect::new( left.calc(cvar.width_base()), top.calc(cvar.height_base()), right.calc(cvar.width_base()), bottom.calc(cvar.height_base()), round );
                     draw!(fill, rect, border);
                 },
                 Drawable::Circle { center, radius, border, fill } => {
-                    let circle = Circle::new( center.calc_rate(width, height), *radius);
+                    let circle = Circle::new( center.calc(&mut cvar), *radius);
                     draw!(fill, circle, border);
                 },
                 Drawable::Ellipse { center, border, elli, fill } => {
-                    let elli = Ellipse::new( center.calc_rate(bounds.width(), bounds.height()), elli.radii(), elli.rotation());
+                    let elli = Ellipse::new( center.calc(&mut cvar), elli.radii(), elli.rotation());
                     draw!(fill, elli, border);
                 },
                 Drawable::Arc{ pos, border, arc} => { 
                     ctx.stroke_styled(arc, &border.color, border.width, &border.style);
                 }
                 Drawable::Line { start, end, style } => {
-                    let start = start.map( |e| e.calc_rate( bounds.width(), bounds.height() ) ).unwrap_or( last_point );
-                    let end = end.calc( start, bounds.width(), bounds.height() );
+                    let start = start.as_ref().map( |e| e.calc( &mut cvar ) ).unwrap_or( last_point );
+                    let end = end.calc( &mut cvar );
                     last_point = end;
                     let style = style.as_ref().unwrap_or( &last_style );
                     //let style = style.as_ref().unwrap_or( &def_stroke );
@@ -328,12 +375,14 @@ impl DrawableStack {
     }
 }
 
+
+
 #[cfg(test)]
 mod test {
     use super::SimpleCalc;
 
     #[test]
-    fn calc_test() {
+    fn simple_calc_test() {
         //add
         let calc = SimpleCalc::parse("calc(90% + 20)").unwrap();
         println!("{:?} {}", calc, calc.calc(100.));
@@ -370,5 +419,38 @@ mod test {
         println!("{:?} {}", calc, calc.calc(10.));
         assert!( calc.calc(10.) == 160. );
         
+    }
+
+    #[test]
+    fn test() {
+        let mut cb = |name: &str, args: Vec<f64>| -> Option<f64> {
+            let mydata: [f64; 3] = [11.1, 22.2, 33.3];
+            match name {
+                // Custom constants/variables:
+                "x" => Some(3.0),
+                "y" => Some(4.0),
+                "100%" => Some(100.0),
+    
+                // Custom function:
+                "sum" => Some(args.into_iter().fold(0.0, |s, f| s + f)),
+    
+                // Custom array-like objects:
+                // The `args.get...` code is the same as:
+                //     mydata[args[0] as usize]
+                // ...but it won't panic if either index is out-of-bounds.
+                "data" => args.get(0).and_then(|f| mydata.get(*f as usize).copied()),
+    
+                // A wildcard to handle all undefined names:
+                _ => None,
+            }
+        };
+    
+        let val = fasteval2::ez_eval("sum(x^2, y^2)^0.5 + data[0] ", &mut cb).unwrap();
+        //                           |   |                   |
+        //                           |   |                   square-brackets act like parenthesis
+        //                           |   variables are like custom functions with zero args
+        //                           custom function
+    
+        assert_eq!(val, 16.1);
     }
 }
